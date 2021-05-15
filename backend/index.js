46919ef14c21b4a7081a2a9c0970d0163a84383d
Daniel Son <dson.email@gmail.com>
2021-05-16 09:48:29 +1000

    Backend. Able to get Users, and create new User.

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,74 @@
 const express = require('express');
 const axios = require('axios');
+const {
+    v4: getId
+} = require('uuid');
 
-const app = express();
 const PORT_NUMBER = 9000;
 const API_DOMAIN = "http://dmmw-api.australiaeast.cloudapp.azure.com:8080";
+const app = express();
+// Be able to parse req.body:
+app.use(express.urlencoded({
+    extended: true
+}));
+
+let users = [{
+        id: 1,
+        name: "Tod Tester",
+        painLevel: 3,
+        diagnosisId: 1,
+    },
+    {
+        id: getId(),
+        name: "Dan",
+        painLevel: 4,
+        diagnosisId: 2,
+    },
+    {
+        id: getId(),
+        name: "Peter",
+        painLevel: 5,
+        diagnosisId: 2,
+    }
+]
 
 const getExternalJSON = url => {
     try {
         return axios.get(url);
     } catch (e) {
         console.error(e);
-        return { data: "Error. Please see the Administrator." };
+        return {
+            data: "Error. Please see the Administrator."
+        };
     }
 }
 
 app.get('/test', (req, res) => {
     console.log("Received /test!");
     res.send("Hello from /test");
+});
+
+// Get all users.
+app.get('/users', (req, res) => {
+    res.send(users);
+});
+
+// Add new user.
+app.post('/users', (req, res) => {
+    const {
+        name,
+        painLevel,
+        diagnosisId
+    } = req.body;
+    const newUser = {
+        id: getId(),
+        name,
+        painLevel,
+        diagnosisId
+    }
+    users.push(newUser);
+    // Redirect to list of users.
+    res.redirect('/users');
 });
 
 // Get a list of illnesses. 
