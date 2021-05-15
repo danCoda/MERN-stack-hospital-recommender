3acc12553e472df856424498df615330c9250f76
Daniel Son <dson.email@gmail.com>
2021-05-16 09:30:36 +1000

    Backend. Preliminary endpoints for /illnesses and /hospital done.

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,38 @@
 const express = require('express');
+const axios = require('axios');
 
 const app = express();
 const PORT_NUMBER = 9000;
+const API_DOMAIN = "http://dmmw-api.australiaeast.cloudapp.azure.com:8080";
+
+const getExternalJSON = url => {
+    try {
+        return axios.get(url);
+    } catch (e) {
+        console.error(e);
+        return { data: "Error. Please see the Administrator." };
+    }
+}
 
 app.get('/test', (req, res) => {
     console.log("Received /test!");
     res.send("Hello from /test");
+});
+
+// Get a list of illnesses. 
+app.get('/illnesses', async (req, res) => {
+    const url = `${API_DOMAIN}/illnesses`;
+    const illnesses = await getExternalJSON(url);
+
+    res.send(illnesses.data);
+});
+
+// Get a list of hospitals. 
+app.get('/hospitals', async (req, res) => {
+    const url = `${API_DOMAIN}/hospitals`;
+    const hospitals = await getExternalJSON(url);
+
+    res.send(hospitals.data);
 });
 
 app.listen(PORT_NUMBER, () => {
